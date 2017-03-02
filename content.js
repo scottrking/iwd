7f8f35fa1bd0301aa11200caf50b070d1cadc89a
Scott King <scottrking@gmail.com>
2017-03-01 20:22:32 -0700

    Added day counting logic for extensions.

--- content.js
+++ content.js
@@ -1,11 +1,20 @@
 var randomNumber = Math.floor(Math.random()*2);
 
+// Time based badassery: count the days from IWD to IWD to make a reference to the badasses in the badass array.
+
+var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+var firstDate = new Date(2017,02,09);
+var secondDate = new Date(2018,02,08);
+
+var diffDays = Math.round((firstDate.getTime() - secondDate.getTime())/(oneDay))+365;
+
+
 //==============================================================
 //BADASSES
 //==============================================================
 
 var badass = new Array();
-// data format for badasses: name (0), link (1), birth/death (2), short bio (3), image (4)
+// data format for badasses: name (0), nickname or sub-head (1), link (2), short bio (3), image (4)
 
 badass[0] = new Array("Elsie MacGill","\"Queenof the Hurricanes\"","https://en.wikipedia.org/wiki/Elsie_MacGill","First female airplaine designer, and the world's first woman to earn an aeronautical engineering degree. She designed war planes, all the while wearing pants. That's right. She contracted polio and was told she would never walk again. Obviously she didn't have time for such bullshit so she engineered herself some canes and kept on being an absolute boss.","img/elsie-macgill.jpg");
 
@@ -17,84 +26,10 @@ badass[1] = new Array("Ethel Catherwood","The \"Prettiest Girl\" at the Games.",
 
 document.addEventListener('DOMContentLoaded', function (event) {
   // replaces elements in layout with a random person from the badass array
-  document.getElementById("badassName").innerHTML = badass[randomNumber][0];
-  document.getElementById("badassLink").setAttribute("href", badass[randomNumber][2]);
-  document.getElementById("badassSub").innerHTML = badass[randomNumber][1];
-  document.getElementById("badassBio").innerHTML = badass[randomNumber][3];
-//  document.getElementById("badassImage").setAttribute("src", badass[randomNumber][4]);
-  document.getElementById("badassPerson").style.backgroundImage = "url("+ badass[randomNumber][4] +")";
-
-
-// All of this code was stolen from https://paintbycode.github.io/gradient-generator/
-
-  //==============================================================
-  //FUNCTION TO CREATE RANDOM INTEGER
-  //==============================================================
-//    function randInt(min, max) {
-//        return Math.floor((Math.random() * max) + min);
-//    }
-
-//  //==============================================================
-//  //RGB GENERATOR
-//  //==============================================================
-//  //random 0 to 255 number for rgb values
-//  var redTop = randInt(0, 255);
-//  var greenTop = randInt(0, 255);
-//  var blueTop = randInt(0, 255);
-//  var rgbTop = "rgb(" + redTop + "," + greenTop + "," + blueTop + ")";
-
-//  var redBottom = randInt(0, 255);
-//  var greenBottom = randInt(0, 255);
-//  var blueBottom = randInt(0, 255);
-//  var rgbBottom = "rgb(" + redBottom + "," + greenBottom + "," + blueBottom + ")";
-
-
-//  var degrees = randInt(0, 360) + "deg";
-
-//  //==============================================================
-//  //REVERSE RGB VALUE FOR CONTRAST TEXT COLOUR
-//  //==============================================================
-//  //get the 'reverse' value for the displayed rgb value
-//  function reverseRgb(rgbValue) {
-//      var revRgbValue = 255 - rgbValue;
-//      return revRgbValue;
-//  }
-
-//  var reverseRedTop = reverseRgb(redTop);
-//  var reverseGreenTop = reverseRgb(greenTop);
-//  var reverseBlueTop = reverseRgb(blueTop);
-//  var reverseRgbTop = "rgb(" + reverseRedTop + "," + reverseGreenTop + "," + reverseBlueTop + ")";
-
-//  var reverseRedBottom = reverseRgb(redBottom);
-//  var reverseGreenBottom = reverseRgb(greenBottom);
-//  var reverseBlueBottom = reverseRgb(blueBottom);
-//  var reverseRgbBottom = "rgb(" + reverseRedBottom + "," + reverseGreenBottom + "," + reverseBlueBottom + ")";
-
-//  //==============================================================
-//  //CONVERT RGB TO HEX
-//  //==============================================================
-//  //convert rgb to hex
-//  function rgbToHex(rgbValue) {
-//      //convert passed number to hexadecimal
-//      var rgbConverted = rgbValue.toString(16);
-//      //prefix a "0" if required
-//      return rgbConverted.length == 1 ? "0" + rgbConverted : rgbConverted;
-//  };
-
-//  var redHexTop = rgbToHex(redTop);
-//  var greenHexTop = rgbToHex(greenTop);
-//  var blueHexTop = rgbToHex(blueTop);
-//  var hexTop = "#" + redHexTop + "" + greenHexTop + "" + blueHexTop;
-
-//  var redHexBottom = rgbToHex(redBottom);
-//  var greenHexBottom = rgbToHex(greenBottom);
-//  var blueHexBottom = rgbToHex(blueBottom);
-//  var hexBottom = "#" + redHexBottom + "" + greenHexBottom + "" + blueHexBottom;
-
-  //==============================================================
-  //SETTING THE MAIN GRADIENT
-  //==============================================================
-//  document.querySelector("body").style.backgroundImage = "linear-gradient(to bottom, " + rgbTop + "," + rgbBottom + ")";
-//  document.querySelector("body").style.color = "white";
+  document.getElementById("badassName").innerHTML = badass[diffDays][0];
+  document.getElementById("badassLink").setAttribute("href", badass[diffDays][2]);
+  document.getElementById("badassSub").innerHTML = badass[diffDays][1];
+  document.getElementById("badassBio").innerHTML = badass[diffDays][3];
+  document.getElementById("badassPerson").style.backgroundImage = "url("+ badass[diffDays][4] +")";
 
 });
