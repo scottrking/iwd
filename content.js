c800bb82cdea817a8c3bbb4062f58047f6af57d0
Hogan Lee <hoganl@criticalmass.com>
2017-03-06 18:16:08 -0700

    SHELBY I PUT IT THERE

--- content.js
+++ content.js
@@ -16,7 +16,7 @@ var diffDays = Math.round((firstDate.getTime() - secondDate.getTime()) / (oneDay
 var badass = new Array();
 // data format for badasses: name (0), nickname or sub-head (1), link (2), short bio (3), image (4)
 
-badass[0] = new Array("Elsie MacGill", "\"Queenof the Hurricanes\"", "https://en.wikipedia.org/wiki/Elsie_MacGill", "First female airplaine designer, and the world's first woman to earn an aeronautical engineering degree. She designed war planes, all the while wearing pants. That's right. She contracted polio and was told she would never walk again. Obviously she didn't have time for such bullshit so she engineered herself some canes and kept on being an absolute boss.", "img/elsie-macgill");
+badass[0] = new Array("Elsie MacGill", "\"Queenof the Hurricanes\"", "https://en.wikipedia.org/wiki/Elsie_MacGill", "First female airplaine designer, and the world's first woman to earn an aeronautical engineering degree. She designed war planes, all the while wearing pants. That's right. She contracted polio and was told she would never walk again. Obviously she didn't have time for such bullshit so she engineered herself some canes and kept on being an absolute boss.", "img/elise-macgill");
 badass[1] = new Array("Ethel Catherwood", "The \"Prettiest Girl\" at the Games.", "https://en.wikipedia.org/wiki/Ethel_Catherwood", "Ethel Catherwood was a kickass, gold-medal winning Saskatoonian who didn't give two hoots about other people's dumb opinions. After kicking unparalleled ass at the 1928 Olympics, Ethel got secretly married and divorced in Reno, while the press were all \"BUT ETHEL!\" and Ethel was like, \"You know what? I don't do interviews.\" This athletic goddess brought home what is still Canada's only track-and-field female gold. Thatta girl, Ethel.", "img/ethel-catherwood");
 badass[2] = new Array("Buffy Sainte Marie", "The Blacklist Slayer", "https://en.wikipedia.org/wiki/Buffy_Sainte-Marie", "Buffy Sante-Marie has a doctorate in getting in everyone\'s comfortable faces. (Actually, it\'s in fine arts, but she's still Dr. Sante-Marie). They tried blacklisting her on the radio, so she went: \"How about I just rock being an iconic folk singer and native activist for four decades?\" Plus she co-wrote \"Up Where We Belong,\" which I\'M NOT CRYING YOU ARE", "img/buffy-sainte-marie", "dark");
 
@@ -26,6 +26,13 @@ badass[2] = new Array("Buffy Sainte Marie", "The Blacklist Slayer", "https://en.
 //==============================================================
 
 document.addEventListener('DOMContentLoaded', function (event) {
+
+    make();
+    document.getElementById("random").addEventListener("click", rando);
+
+});
+
+function make(){
     // replaces elements in layout with a random person from the badass array
     document.getElementById("badassName").innerHTML = badass[randomNumber][0];
     document.getElementById("badassLink").setAttribute("href", badass[randomNumber][2]);
@@ -40,21 +47,15 @@ document.addEventListener('DOMContentLoaded', function (event) {
     }
 
     if(badass[randomNumber][5]){
-        console.log("hello world");
+        document.getElementById("badassPerson").classList.remove('light');
         document.getElementById("badassPerson").classList.add('dark');
-        console.log("ADSFASDFASDFASDF");
+    }else{
+        document.getElementById("badassPerson").classList.add('light');
+        document.getElementById("badassPerson").classList.remove('dark');
     }
-
-    document.getElementById("random").addEventListener("click", rando);
-
-});
+}
 
 function rando() {
     randomNumber = Math.floor(Math.random() * 2);
-    document.getElementById("badassName").innerHTML = badass[randomNumber][0];
-    document.getElementById("badassLink").setAttribute("href", badass[randomNumber][2]);
-    document.getElementById("badassSub").innerHTML = badass[randomNumber][1];
-    document.getElementById("badassBio").innerHTML = badass[randomNumber][3];
-    document.getElementById("badassPerson").style.backgroundImage = "url(" + badass[randomNumber][4] + ")";
-
+    make();
 }
